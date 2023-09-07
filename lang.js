5a47556918a418b2819031aef9374485bfdd25cc
D-S1808 <133581935+D-S1808@users.noreply.github.com>
2023-09-07 23:56:37 +0200

    change DE root / nav padding
    ToDo:
    nav follow scroll (IT & DE)
    lang.js redo
    select lang from device settings

--- lang.js
+++ lang.js
@@ -2,5 +2,6 @@ let language = "IT"
 
 function setLanguage(lang) {
     language = lang;
-    window.location.href = `/${language}/index.html`;
+    window.location.href = `/Users/domenicoscazziota/Documents/GitHub/websiteChiesa/DE/index.html#`;
+    //window.location.href = `/${language}/index.html`;
 }
