128d33564de793111a463e64751ac4222653ff76
D-S1808 <133581935+D-S1808@users.noreply.github.com>
2023-09-28 23:26:35 +0200

    small changes

--- lang.js
+++ lang.js
@@ -1,7 +1,8 @@
 let language = "IT"
+let language2 = "DE"
 
 function setLanguage(lang) {
     language = lang;
-    window.location.href = `/Users/domenicoscazziota/Documents/GitHub/websiteChiesa/DE/index.html#`;
+    window.location.href = `/Users/domenicoscazziota/Documents/GitHub/websiteChiesa/DE/index.html`;
     //window.location.href = `/${language}/index.html`;
 }
